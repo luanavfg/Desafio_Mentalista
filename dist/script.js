9c21248d87cabdb876074850bf87e6c1b1d518df
luanavfg <luanavefago1@gmail.com>
2022-04-04 11:05:49 -0300

    Incluir Códigos

---
+++ dist/script.js
@@ -0,0 +1,16 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+
+function Chutar() {
+  var elementoResultado = document.getElementById("resultado");
+  var chute = parseInt(document.getElementById("valor").value);
+  console.log(chute);
+  if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = "Você acertou";
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
+  } else if (chute > numeroSecreto) {
+    elementoResultado.innerHTML = "Você errou, tente um número menor";
+  } else if (chute < numeroSecreto) {
+    elementoResultado.innerHTML = "Você errou, tente um número maior";
+  }
+}
